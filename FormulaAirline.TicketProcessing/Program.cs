30df2fbc635347273aa150d6deb2065799945845
Chidiebere Alfred Levi <46676018+rapXidy@users.noreply.github.com>
2023-04-12 08:46:04 +0100

    Added the console App (consumer)

---
+++ FormulaAirline.TicketProcessing/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!, Welcome to the ticketing service!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "mypass",
+    VirtualHost="/"
+};
+
+var conn = factory.CreateConnection();
+
+using var channel = conn.CreateModel(); //enables us create a new channel
+
+channel.QueueDeclare("bookings", durable: true, exclusive: false);
+
+var consumerr = new EventingBasicConsumer(channel);
+
+consumerr.Received += (model, eventArgs) =>
+{
+    //getting my byte[]
+    var body = eventArgs.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($@"New ticket procceing is initiated for - {message}");
+}; // here we'll be having anonymous arguments
+
+channel.BasicConsume("bookings",true,consumerr);
+
+Console.ReadKey();
